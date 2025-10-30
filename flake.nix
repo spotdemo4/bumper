15a2e900e4910046abf10a5fe88a41acc0fc3d5f
trev <me@trev.xyz>
2025-10-30 13:22:16 -0400

    bump: v0.1.4 -> v0.1.5
    [skip ci]:

--- flake.nix
+++ flake.nix
@@ -90,7 +90,7 @@
 
       packages.default = pkgs.stdenv.mkDerivation (finalAttrs: {
         pname = "bumper";
-        version = "0.1.4";
+        version = "0.1.5";
         src = ./.;
 
         nativeBuildInputs = with pkgs; [
