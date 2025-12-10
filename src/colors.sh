d43c04b240fd4e4df292cfda6e97a52c81554e20
spotdemo4 <me@trev.xyz>
2025-12-10 11:58:44 -0500

    fix: default unbounded vars

--- src/colors.sh
+++ src/colors.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # set TERM to linux in CI environments for tput compatibility
-if [[ -n "${CI-}" ]]; then
+if [[ -n "${CI-}" || -z "${TERM-}" ]]; then
     TERM=linux
 fi
 
@@ -21,17 +21,17 @@ if colors=$(tput -T "${TERM}" colors 2> /dev/null); then
 fi
 
 function bold {
-    printf "%s%s%s\n" "${color_bold}" "$1" "${color_reset}"
+    printf "%s%s%s\n" "${color_bold-}" "$1" "${color_reset-}"
 }
 
 function info {
-    printf "%s%s%s\n" "${color_info}" "$1" "${color_reset}"
+    printf "%s%s%s\n" "${color_info-}" "$1" "${color_reset-}"
 }
 
 function warn {
-    printf "%s%s%s\n" "${color_warn}" "$1" "${color_reset}"
+    printf "%s%s%s\n" "${color_warn-}" "$1" "${color_reset-}"
 }
 
 function success {
-    printf "%s%s%s\n" "${color_success}" "$1" "${color_reset}"
+    printf "%s%s%s\n" "${color_success-}" "$1" "${color_reset-}"
 }
