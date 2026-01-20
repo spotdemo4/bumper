5bfe361e2a8c514e0659d8b79428cd07e688d95f
spotdemo4 <me@trev.xyz>
2026-01-20 17:55:43 -0500

    fix: manual bump

--- flake.nix
+++ flake.nix
@@ -172,7 +172,7 @@
         packages = {
           default = pkgs.stdenv.mkDerivation (finalAttrs: {
             pname = "bumper";
-            version = "0.8.3";
+            version = "0.9.0";
 
             src = fs.toSource {
               root = ./.;
