6d845048913704e2cd70a22e9040fed01f53edcf
trev <me@trev.xyz>
2026-03-30 21:00:45 -0400

    bump: v0.14.0 -> v0.14.1

--- flake.nix
+++ flake.nix
@@ -170,7 +170,7 @@
         packages = pkgs.mkPackages pkgs (pkgs: {
           default = pkgs.rustPlatform.buildRustPackage (finalAttrs: {
             pname = "bumper";
-            version = "0.14.0";
+            version = "0.14.1";
 
             src = pkgs.lib.fileset.toSource {
               root = ./.;
