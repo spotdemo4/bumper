1b050707ed02df899ddf968306487eaab1079aa5
doctor <doctor@trev.xyz>
2026-08-11 23:47:41 +0000

    bump: v0.25.0 -> v0.26.0

--- flake.nix
+++ flake.nix
@@ -106,7 +106,7 @@
           default = pkgs.rustPlatform.buildRustPackage (
             final: with pkgs.lib; {
               pname = "bumper";
-              version = "0.25.0";
+              version = "0.26.0";
 
               src = fileset.toSource {
                 root = ./.;
